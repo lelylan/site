7c819d9d9a51c884a36bb985e7678268316b25eb
Andrea Reginato <andrea.reginato@gmail.com>
2011-07-12 10:55:00 +0200

    Added blog structure

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,28 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Breadcrumbs
+include Nanoc3::Helpers::Capturing
+include Nanoc3::Helpers::Filtering
+include Nanoc3::Helpers::HTMLEscape
+include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Text
+include Nanoc3::Helpers::XMLSitemap
+
+class Nanoc3::Item
+
+  # Get the item content
+  def content(opts = {})
+    opts[:rep] ||= :default
+    opts[:snapshot] ||= :last
+    reps.find { |r| r.name == opts[:rep] }.content_at_snapshot(opts[:snapshot])
+  end
+
+  # Get the item name
+  #
+  #   /blog/domotics-explained   # domotics-explained
+  def name
+    identifier.split("/").last 
+  end
+end
+
